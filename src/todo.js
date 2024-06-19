8d2880ed8cd3f9db548d05136e67efa6739f701d
krish01j <krishj518@gmail.com>
2024-06-19 12:32:04 +0530

    aa

--- src/todo.js
+++ src/todo.js
@@ -1,14 +1,22 @@
-import react from 'react'
+import react ,{useState}from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
 import { faTrash} from '@fortawesome/free-solid-svg-icons' 
 import './todo.css'
 
+
+
 const Todo = ({task}) =>{
+    const[check,changeVal]=useState(true)
+
+   const changeClass = () =>{changeVal(!check)
+        console.log(check)
+    }
+    
     return(
         <div >
-            <div className='container2'>  
-            <input className='tick' type='checkbox'></input>  
+            <div className={check ? "container2":"container3" }>  
+            <input className='tick' type='checkbox' onClick={changeClass} ></input>  
             <p>{task.task} </p>
             <FontAwesomeIcon  className='trash' icon={faTrash} />
             </div>
